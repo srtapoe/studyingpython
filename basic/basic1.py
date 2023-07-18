d0c672cacf6b99ffe999bd82a3bd0c7274003d40
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2023-07-17 22:21:45 -0300

    Adicionado desafio básico 1

--- basic/basic1.py
+++ basic/basic1.py
@@ -1,1 +1,9 @@
-print("Hello")
+# data atual
+from datetime import date, timedelta
+
+data_hoje = date.today()
+
+data_futura = data_hoje + timedelta(+2)
+
+print(f'A data de hoje eh {data_hoje}')
+print(f'A data daqui 2 dias será {data_futura}')
