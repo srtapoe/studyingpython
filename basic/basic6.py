9692ad88a9d90dc0c0652a1458f9e4713a8584e7
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2023-07-19 22:48:24 -0300

    Adicionada solução do exercício básico 6

---
+++ basic/basic6.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número: "))
+
+numeroFloat = "{:.2f}".format(float(numero))
+
+print(f'Número digitado: {numero} \n'
+      f'Número com casas decimais: {numeroFloat}')
